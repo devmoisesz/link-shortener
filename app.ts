87732e86f4685e8c9de47db305bdceee1cc2bf02
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-04 20:42:39 -0300

    feat: express rate limit

--- app.ts
+++ app.ts
@@ -2,12 +2,19 @@ import 'dotenv/config';
 import express from 'express';
 import { connectDatabase } from './config/database';
 import shortUrlRoutes from './src/routes/short-url.routes'
+import rateLimit from 'express-rate-limit'
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 100, // maximo 100 requisições por IP
+  message: 'Muitas requisições, tente novamente mais tarde'
+});
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/shortener', shortUrlRoutes)
+app.use('/shortener', shortUrlRoutes, limiter)
 
 connectDatabase();
 
