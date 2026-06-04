0e51cbdf7b58460303868ed2dafbe6a42e656b13
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-04 20:28:03 -0300

    fix: Handling Inadequate Errors

--- src/controllers/short-url.controller.ts
+++ src/controllers/short-url.controller.ts
@@ -23,14 +23,18 @@ async function shortenUrl(req: Request, res: Response) {
 
 async function redirectLink(req: Request<RedirectParams>, res: Response) {
     try {
-        const shortCode = req.params.shortCode
-        const url = await service.redirectLink(shortCode)
-        res.redirect(url)
+        const shortCode = req.params.shortCode;
+        const url = await service.redirectLink(shortCode);
+        res.redirect(url);
     } catch (error: unknown) {
-         return res.status(500).json({
-            message: error instanceof Error
-                ? error.message
-                : 'Erro interno do servidor'
+        if (error instanceof Error && error.message === 'URL não encontrada') {
+            return res.status(404).json({
+                message: 'URL encurtada não encontrada',
+                shortCode: req.params.shortCode
+            });
+        }
+        return res.status(500).json({
+            message: 'Erro interno do servidor'
         });
     }
 }
