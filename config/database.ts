1ae14f683ffb099a57f3246fc10d00f88477eaa3
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-06 20:55:33 -0300

    npm install -D tsx

--- config/database.ts
+++ config/database.ts
@@ -8,9 +8,8 @@ export async function connectDatabase(): Promise<void> {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
 
-    console.log('✅ Conectado ao MongoDB');
   } catch (error) {
-    console.error('❌ Erro ao conectar ao MongoDB:', error);
+    console.error('Erro ao conectar ao MongoDB:', error);
 
     process.exit(1);
   }
