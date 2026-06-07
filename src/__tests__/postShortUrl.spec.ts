ddf73ca4e03e9cee85eaf74ad433d1a70fd3ea97
Moisés Figueiredo <moisesfigueiredo2508@gmail.com>
2026-06-07 15:07:53 -0300

    test: post-API/shorten integration test

---
+++ src/__tests__/postShortUrl.spec.ts
@@ -0,0 +1,27 @@
+import app from "../../app";
+import req from 'supertest';
+import { describe, it, expect, beforeAll, afterAll } from "vitest";
+import 'dotenv/config';
+import { connectDatabase } from "../../config/database";
+import mongoose from "mongoose";
+
+const token = process.env.TOKEN_TESTING
+
+describe('POST /shortener/api/shorten', () => {
+    beforeAll (async() => {
+        connectDatabase()
+    })
+    it('deve retornar link encurtado', async () => {
+        const res = await req(app)
+        .post('/shortener/api/shorten')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+            longUrl: "https://github.com/devmoisesz"
+        });
+
+        expect(res.status).toBe(201)
+    });
+    afterAll(async() => {
+        mongoose.disconnect()
+    })
+});
